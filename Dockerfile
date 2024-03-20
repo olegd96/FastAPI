1a46705aff6ccc6dcbbd8282e7712fb0760d22c0
olegd96 <chepalin@yandex.ru>
2024-03-21 01:57:54 +0500

    daily commit

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,8 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-RUN chmod a+x /booking/docker/*.sh
+# RUN chmod a+x /booking/docker/*.sh
 
-CMD ["gunicorn", "app.main:app", "--workers", "1", "--worker-class", "uvicorn.workers.UvicornWorker", "--bind=0.0.0.0:8000"]
+# CMD ["gunicorn", "app.main:app", "--workers", "1", "--worker-class", "uvicorn.workers.UvicornWorker", "--bind=0.0.0.0:8000"]
 
 
--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -8,29 +8,29 @@ celery = Celery(
     broker=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
     broker_connection_retry_on_startup = True,
     include=["app.tasks.tasks",
-            #  "app.tasks.scheduled",
+             "app.tasks.scheduled",
              ]
 )
 
-# celery.conf.beat_schedule = {
-#     "app_periodic_tasks": {
-#         "task": "notice_one_day", 
-#         #"schedule": 60,
-#         "schedule": schedules.crontab(minute="37", hour="19"),
-#     },
+celery.conf.beat_schedule = {
+    "app_periodic_tasks": {
+        "task": "notice_one_day", 
+        #"schedule": 60,
+        "schedule": schedules.crontab(minute="37", hour="19"),
+    },
 
-#     "app_periodic_tasks_1": {
-#         "task": "notice_three_days",
-#         "schedule": schedules.crontab(minute="37", hour="20")
-#     },
+    "app_periodic_tasks_1": {
+        "task": "notice_three_days",
+        "schedule": schedules.crontab(minute="37", hour="20")
+    },
 
-#     "app_periodic_tasks_2": {
-#         "task": "delete_old_token",
-#         "schedule": schedules.crontab(minute="00", hour="23")
-#     },
+    "app_periodic_tasks_2": {
+        "task": "delete_old_token",
+        "schedule": schedules.crontab(minute="00", hour="23")
+    },
 
-#     "app_periodic_tasks_3": {
-#         "task": "delete_old_book_from_cart",
-#         "schedule": schedules.crontab(minute="00", hour="23", day_of_month="1")
-#     },
-# }
+    "app_periodic_tasks_3": {
+        "task": "delete_old_book_from_cart",
+        "schedule": schedules.crontab(minute="00", hour="23", day_of_month="1")
+    },
+}

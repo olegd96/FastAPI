556d990fe93ad9530f92f76e8325f7781f850771
olegd96 <chepalin@yandex.ru>
2024-03-30 01:11:05 +0500

    daily commit

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ else:
     DATABASE_URL = settings.DATABASE_URL
     DATABASE_PARAMS = {}
 
-engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
+engine = create_async_engine(DATABASE_URL, pool_pre_ping=True, **DATABASE_PARAMS)
 engine_nullpool = create_async_engine(DATABASE_URL, **{"poolclass": NullPool})
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 async_session_taskmaker = async_sessionmaker(bind=engine, expire_on_commit=False)

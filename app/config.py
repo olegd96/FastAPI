e40ecf7d8039da51af04546a66c8c4478cafdff8
olegd96 <chepalin@yandex.ru>
2024-04-23 00:44:21 +0500

    daily commit

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@ from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-
 class Settings(BaseSettings):
     MODE: Literal["DEV", "TEST", "PROD"]
     LOG_LEVEL: Literal["DEBUG", "INFO"]
@@ -43,7 +42,7 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
 
-    model_config = SettingsConfigDict(env_file=".env-non-dev")
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()

69b319bdee44e062bcdd273980675e1417d42283
olegd96 <chepalin@yandex.ru>
2024-04-24 02:33:17 +0500

    dc

--- app/config.py
+++ app/config.py
@@ -42,7 +42,7 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env-non-dev")
 
 
 settings = Settings()

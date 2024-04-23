a321bd4391b66122de8e7ffe5e3dc772e33c48e8
olegd96 <chepalin@yandex.ru>
2024-04-24 04:08:49 +0500

    dc

--- app/images/router.py
+++ app/images/router.py
@@ -1,7 +1,7 @@
 import aiofiles
 from fastapi import APIRouter, UploadFile
 import shutil
-
+from pathlib import Path
 from app.tasks.tasks import process_pic
 
 
@@ -14,9 +14,7 @@ router = APIRouter(
 
 @router.post("/hotels")
 async def add_hotel_images(name: int, file: UploadFile):
-    im_path = f"app/static/images/{name}.webp"
+    im_path = f"app\static\images\{name}.webp"
     with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
     process_pic.delay(im_path)
-
-    

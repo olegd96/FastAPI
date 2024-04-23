1c403e0e594d51483939cac63e81efa2236d5c0c
olegd96 <142448201+olegd96@users.noreply.github.com>
2024-04-23 18:32:12 +0500

    Update router.py

--- app/images/router.py
+++ app/images/router.py
@@ -14,7 +14,7 @@ router = APIRouter(
 
 @router.post("/hotels")
 async def add_hotel_images(name: int, file: UploadFile):
-    im_path = f"./static/images/{name}.webp"
+    im_path = f"/app/static/images/{name}.webp"
     with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
     process_pic.delay(im_path)
